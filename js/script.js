813a6086b041a6380ac4594c011279d1f695a969
mohammad-rifat <mohammadrifat7717@gmail.com>
2023-04-19 18:19:03 +0600

    added all files and folders

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(function () {
+  "use strict";
+  $(".movie_slider").slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    arrows: false,
+    centerPadding: "45px",
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
